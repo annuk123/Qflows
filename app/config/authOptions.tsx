c1839b3a3cbb06abfde83146fe1576584bf65759
Annu Kumari <anuk35168@gmail.com>
2024-12-07 09:06:19 +0530

    commit changes to authOptions.tsx

--- app/config/authOptions.tsx
+++ app/config/authOptions.tsx
@@ -31,8 +31,8 @@ declare module 'next-auth' {
 export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
     }),
 
     CredentialsProvider({
