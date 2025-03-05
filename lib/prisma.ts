3360fb98b9b3b47db1481e25dda37e6e4e9dd6a6
Annu Kumari <anuk35168@gmail.com>
2025-03-05 22:19:39 +0530

    test

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ["query"], // Logs queries for debugging
+ // log: ["query"], // Logs queries for debugging
 });
 
 // Ensure we only create one instance in development to avoid Next.js hot-reloading issues
